839794df2a039103fff0bbad2ee29dbf06d3d076
Sébastien Gérard <me@sgerard.be>
2023-12-26 19:00:37 +0100

    Rest client

--- src/main/java/be/sgerard/test/spring6/DemoController.java
+++ src/main/java/be/sgerard/test/spring6/DemoController.java
@@ -1,7 +1,13 @@
 package be.sgerard.test.spring6;
 
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
+
+import java.util.Map;
 
 @RestController
 public class DemoController {
@@ -9,5 +15,31 @@ public class DemoController {
     @GetMapping("/")
     public String hello() {
         return "Hello World!";
+    }
+
+    @GetMapping("/rest-client")
+    public String restClient() {
+        var url = "https://opentdb.com/api.php?amount={amount}&category={category}";
+
+        final ResponseEntity<Map<String, Object>> entity = RestClient.create()
+                .get()
+                .uri(url, 1, 18)
+                .retrieve()
+                .onStatus(
+                        HttpStatusCode::is4xxClientError,
+                        (request, response) -> {
+                            throw new IllegalArgumentException("Why you didn't do it properly?");
+                        }
+                )
+                .onStatus(
+                        HttpStatusCode::is5xxServerError,
+                        (request, response) -> {
+                            throw new IllegalArgumentException("They failed!");
+                        }
+                )
+                .toEntity(new ParameterizedTypeReference<>() {
+                });
+
+        return entity.getBody() + "\n" + entity.getStatusCode() + "\n" + entity.getHeaders();
     }
 }
